edfca699f6d1a3f64bbf09a66554061cd1233d4c
wanghuipeng <624793769@qq.com>
2018-08-26 15:19:00 +0800

    feat:test.env

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
+    API_ROOT: '"http://192.168.1.128:1030/fjs/"',
+    API_TOKEN: '"Bearer "'
 })
--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+    NODE_ENV: '"testing"',
+    API_ROOT: '"http://192.168.1.128:1030/fjs/"',
+    API_TOKEN: '"Bearer "'
 })
